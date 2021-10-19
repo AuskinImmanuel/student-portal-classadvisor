b8066eb300c0446716570014cc065ab20cc48535
Sanjay03K <75990210+Sanjay03K@users.noreply.github.com>
2021-10-19 02:22:57 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(
       <Route path={`/auth`} component={AuthLayout} />
       <Route path={`/admin`} component={AdminLayout} />
 
-      <Redirect from={`/`} to="/admin/SignIn" />
+      <Redirect from={`/`} to="/auth/SignIn" />
     </Switch>
   </HashRouter>,
   document.getElementById("root")
